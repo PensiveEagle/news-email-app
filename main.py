b914bc3d15023cf1f757d8cf34f1733e4cd35372
PensiveEagle <halljf96@gmail.com>
2026-01-02 11:31:32 +0000

    Print article titles in the CLI
    Loop through the articles and print out the titles

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ api_key = os.getenv( "API_KEY" )
 url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-12-02&sortBy=publishedAt&apiKey={api_key}"
 
 response = requests.get( url )
+content = response.json()
 
-print( response.text )
+for article in content["articles"]:
+    print( article["title"] )
