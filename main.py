b64f4e835770919245af785afd21b72a79bfc34b
PensiveEagle <halljf96@gmail.com>
2026-01-02 11:07:20 +0000

    Added API request
    Added an API request to newsapi.org
    Filterning to Tesla news stories
    Using API key stored in env var

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv( "API_KEY" )
+
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-12-02&sortBy=publishedAt&apiKey={api_key}"
+
+response = requests.get( url )
+
+print( response.text )
